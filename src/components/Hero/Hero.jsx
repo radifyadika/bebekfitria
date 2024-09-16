61aff689c405c62e719380a918a8d2a238e3bb80
radifyadika <radiframadhan@gmail.com>
2024-09-16 20:24:32 +0700

    custom domain

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -12,7 +12,13 @@ const Hero = () => {
           Anda produk telur bebek berkualitas tinggi, memastikan produktivitas
           maksimal, semua dengan harga yang sangat terjangkau!
         </p>
-        <button className="btn">
+        <button
+          className="btn"
+          onClick={() =>
+            (window.location.href =
+              "https://wa.me/6281310055488?text=Halo%20Bebek%20Fitria..%0APerkenalkan%20saya%2C%20%0A%0ANama%20%3A%20%0ADomilisi%20%2F%20Wilayah%20%3A%20%0A%0AMau%20bertanya%20dong%20min%20%3F")
+          }
+        >
           Pesan Sekarang <img src={dark_arrow} alt="" />
         </button>
       </div>
